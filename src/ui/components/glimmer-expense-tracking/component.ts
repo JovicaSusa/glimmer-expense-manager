57aa65f17ce05b5fa58547cd7bb822461d116093
Jovica Susa <jovicasusa1985@gmail.com>
2017-05-26 13:00:06 +0200

    Changed expense array to Set

--- src/ui/components/glimmer-expense-tracking/component.ts
+++ src/ui/components/glimmer-expense-tracking/component.ts
@@ -3,10 +3,10 @@ import Component, { tracked } from "@glimmer/component";
 export default class GlimmerExpenseTracking extends Component {
   infoBoxTitles = ['Credit', 'Debit', 'Balance'];
   amount = 0;
-  @tracked expenses = [];
+  @tracked expenses = new Set([]);
 
   addExpense(expense) {
-    this.expenses.push(expense);
+    this.expenses.add(expense);
     this.expenses = this.expenses;
   };
 }
